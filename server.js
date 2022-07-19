8405dfe4c1764e1f35b8d22088938500de43f284
CameronTwiggs <cameron.020617@gmail.com>
2022-07-19 04:12:16 -0400

    fixes port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mysql = require('mysql');
 
 // starts express server
 const app = express();
-const port = 8080 || process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 
