979c687d691bf65bd084049378b5ef7557fb2c77
CameronTwiggs <cameron.020617@gmail.com>
2022-07-21 04:20:34 -0400

    fixes styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,22 +3,22 @@ export default function Footer() {
         <footer>
         <ul className="footer__list">
           <li>
-            <a href="#">
+            <a href="www.youtube.com">
               <img src="../images/social/github.svg" alt="Github" />
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="www.youtube.com">
               <img src="../images/social/facebook.svg" alt="Facebook" />
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="www.youtube.com">
               <img src="../images/social/instagram.svg" alt="Instagram" />
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="www.youtube.com">
               <img src="../images/social/twitter.svg" alt="Facebook" />
             </a>
           </li>
