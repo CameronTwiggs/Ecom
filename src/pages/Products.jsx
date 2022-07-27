0906f1fd97098679d0fc4f7e87d6097fbcd6e7f5
CameronTwiggs <cameron.020617@gmail.com>
2022-07-26 23:19:54 -0400

    bruh

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,19 +4,23 @@ import { useEffect, useState } from "react";
 
 export default function Products() {
     const [products, setProducts] = useState([]);
+    const [filter, setFilter] = useState("");
     useEffect(() => {
-        fetch("https://r2h-ecom-backend.herokuapp.com/")
-            .then(response => response.json())
-            .then(data => {
-                setProducts(data);
-            })
-    }, [])
+        const fetchData = async () => {
+            const result = await fetch(`https://r2h-ecom-backend.herokuapp.com/${filter}`)
+            const data = await result.json();
+            setProducts(data);
+        }
+        fetchData();
+    }, [filter])
+
+    
     return (
         <section className="main__section product__main">
             <h1 className="main__title terminal terminal--purple">~$ Shop...</h1>
             <input type="text" />
-            <button onClick={() => console.log("Low To High")}></button>
-            <button onClick={() => console.log("High To Low")}></button>
+            <button onClick={() => setFilter("low")}>Low</button>
+            <button onClick={() => setFilter("high")}>High</button>
             <ul className="product__list">
                 {products.map(product => {
                     return <ProductCard key={product.id} products={product} settings={"li"}/>
