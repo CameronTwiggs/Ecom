1231c5ad8971e9eb815a982b19030f11043327d8
CameronTwiggs <cameron.020617@gmail.com>
2022-07-26 22:39:57 -0400

    bruh

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,5 @@
 
 import ProductCard from "../components/ProductCard.jsx"
-
 import { useEffect, useState } from "react";
 
 export default function Products() {
@@ -16,11 +15,12 @@ export default function Products() {
         <section className="main__section product__main">
             <h1 className="main__title terminal terminal--purple">~$ Shop...</h1>
             <input type="text" />
+            <button onClick={() => console.log("Low To High")}></button>
+            <button onClick={() => console.log("High To Low")}></button>
             <ul className="product__list">
                 {products.map(product => {
                     return <ProductCard key={product.id} products={product} settings={"li"}/>
-                }
-                )}
+                })}
             </ul>
         </section>
     )
